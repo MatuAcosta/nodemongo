992bed6fef2b7899f409c7795b35dbcc237917b0
MatuAcosta <acosta_matias8@hotmail.com>
2021-12-14 11:09:53 -0300

    Variables de entorno

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require ('express');
 const app = express()
-const port = process.env.PORT || 3000;
+
+//dotenv para poder ocultar variables de entorno
+require('dotenv').config()
+const port = process.env.PORT;
 
 //conexion a una bd de mongo a traves de mongoose
 const mongoose = require('mongoose');
 
-const user = 'usuario2';
+//ESTOS VALORES OCULTAREMOS UTILIZANDO DOTENV
+/*const user = 'usuario2';
 const password = 'olakase123';
-const dbname = 'veterinaria'
+const dbname = 'veterinaria'*/
 // url de conexion
-const uri = `mongodb+srv://${user}:${password}@cluster0.6ce2y.mongodb.net/${dbname}?retryWrites=true&w=majority`; 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.6ce2y.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`; 
 
 mongoose.connect(uri)
 .then(()=> console.log('Base de datos configurada'))
